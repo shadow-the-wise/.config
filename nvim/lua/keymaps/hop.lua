2614bf36499bb9e8ff54444af59764ca1191df5c
shadowchaser04 <shadowchasing@icloud.com>
2023-01-24 03:04:22 +0000

    ADDED: hop key mappings

--- nvim/lua/keymaps/hop.lua
+++ nvim/lua/keymaps/hop.lua
@@ -1,6 +1,46 @@
 local key_map = vim.api.nvim_set_keymap
+local hop = require("hop")
+local directions = require('hop.hint').HintDirection
 
-key_map("n", "\\", "<CMD>HopChar1<CR>", { silent = true, desc = "Hop to word" })
+-- Search after the cursor
 
+vim.keymap.set('n', 'F',
+    function()
+        hop.hint_char1({
+            direction = directions.AFTER_CURSOR,
+            current_line_only = false
+        })
+    end,
+    {
+        remap = true,
+        desc = "Hop after the cursor"
+    }
+)
 
+-- Search before the cursor
+
+vim.keymap.set('n', 'f',
+    function() hop.hint_char1({
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = false
+    })
+    end,
+    {
+        remap = true,
+        desc = "Hop before the cursor"
+    }
+)
+
+-- Search for pattern
+
+vim.keymap.set('n', '<leader>f',
+    function() hop.hint_patterns({
+        current_line_only = false
+    })
+    end,
+    {
+        remap = true,
+        desc = "Hop pattern"
+    }
+)
 
