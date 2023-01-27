31d37798e68bec085082a2c40c5f147c92d1f0b6
shadowchaser04 <shadowchasing@icloud.com>
2023-01-27 07:42:55 +0000

    ADDED: spell settings for text and markdown

--- nvim/lua/commands/spell.lua
+++ nvim/lua/commands/spell.lua
@@ -1,6 +1,11 @@
 -- Enable spell checking for text based file types
 
+local SpellGroup = vim.api.nvim_create_augroup("SpellGroup", { clear = true })
+
+-- Turn on spell checking for text based filetypes
+
 vim.api.nvim_create_autocmd("FileType", {
+    group = SpellGroup,
     desc = 'Set spell for filetypes of a text based language',
     pattern = { "*.txt", "*.md", "*.tex", "*.text", "markdown" },
     callback = function()
@@ -14,6 +19,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Turn off spell checking when changing buffer or window
 
 vim.api.nvim_create_autocmd({ "BufLeave", "BufHidden", "WinLeave" }, {
+    group = SpellGroup,
     desc = 'Set no spell when leaveing a buffer or window',
     callback = function()
         vim.opt.spell = false
