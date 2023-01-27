1f85b5da342a93a9f58f98022f2b72053dfca877
shadowchaser04 <shadowchasing@icloud.com>
2023-01-27 07:41:04 +0000

    ADDED: quit group for closing quickfix, man and help pages

--- nvim/lua/commands/quits.lua
+++ nvim/lua/commands/quits.lua
@@ -1,6 +1,10 @@
+local QuitGroup = vim.api.nvim_create_augroup("QuitGroup", { clear = true })
+
 -- Q quit help and quickfix
 
 vim.api.nvim_create_autocmd("FileType", {
+    desc = "Close help and quickfix window with the q key",
+    group = QuitGroup,
     pattern = { "help", "qf" },
     command = [[ nnoremap <buffer><silent>q <CMD>close<CR> ]]
 })
@@ -8,6 +12,8 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Q quit man page
 
 vim.api.nvim_create_autocmd("FileType", {
+    desc = "Close man page window with the q key",
+    group = QuitGroup,
     pattern = { "man" },
     command = [[ nnoremap <buffer><silent>q <CMD>quit<CR> ]]
 })
