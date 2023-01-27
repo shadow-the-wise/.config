3559333569798844bd65632420b75e68d06e08d0
shadowchaser04 <shadowchasing@icloud.com>
2023-01-27 07:41:27 +0000

    ADDED: skeleton template files

--- nvim/lua/commands/skeletons.lua
+++ nvim/lua/commands/skeletons.lua
@@ -1,29 +1,33 @@
 -- Skeletons
 
+-- Skeletons are template files that are ran when a file is loaded.
+
+local SkeletonGroup = vim.api.nvim_create_augroup("SkeletonTemplates", {clear = true})
+
 vim.api.nvim_create_autocmd("BufNewFile", {
     desc = 'create a template file for ruby filetypes',
+    group = SkeletonGroup,
     pattern = "*.rb",
-    group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
     command = "0r ~/.config/nvim/skeleton/skeleton.rb"
 })
 
 vim.api.nvim_create_autocmd("BufNewFile", {
     desc = 'create a template file for text and markdown filetypes',
+    group = SkeletonGroup,
     pattern = { "*.txt", "*.md" },
-    group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
     command = "0r ~/.config/nvim/skeleton/skeleton.md"
 })
 
 vim.api.nvim_create_autocmd("BufNewFile", {
     desc = 'create a template file for bash filetypes',
+    group = SkeletonGroup,
     pattern = "*.sh",
-    group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
     command = "0r ~/.config/nvim/skeleton/skeleton.sh"
 })
 
 vim.api.nvim_create_autocmd("BufNewFile", {
     desc = 'create a template file for peal filetypes',
+    group = SkeletonGroup,
     pattern = "*.pl",
-    group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
     command = "0r ~/.config/nvim/skeleton/skeleton.pl"
 })
