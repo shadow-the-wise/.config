e4e5463eb0879517c2c72527eb1ec475fbf9064f
shadowchaser04 <shadowchasing@icloud.com>
2023-01-28 04:19:29 +0000

    ADDED: complition working fully tab and s-tab are now fine

--- nvim/plugin/nvim_cmp.lua
+++ nvim/plugin/nvim_cmp.lua
@@ -5,7 +5,50 @@ local cmp = require 'cmp'
 -- Require lua snippet Engine
 local luasnip = require("luasnip")
 
+-- Set completeopt to have a better completion experience
+
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+
+
+local icons = {
+    Array = ' ',
+    Boolean = ' ',
+    Class = ' ',
+    Color = ' ',
+    Constant = ' ',
+    Constructor = ' ',
+    Enum = ' ',
+    EnumMember = ' ',
+    Event = ' ',
+    Field = ' ',
+    File = ' ',
+    Folder = ' ',
+    Function = ' ',
+    Interface = ' ',
+    Key = ' ',
+    Keyword = ' ',
+    Method = ' ',
+    Module = ' ',
+    Namespace = ' ',
+    Null = 'ﳠ ',
+    Number = ' ',
+    Object = ' ',
+    Operator = ' ',
+    Package = ' ',
+    Property = ' ',
+    Reference = ' ',
+    Snippet = ' ',
+    String = ' ',
+    Struct = ' ',
+    Text = ' ',
+    TypeParameter = ' ',
+    Unit = ' ',
+    Value = ' ',
+    Variable = ' ',
+}
+
 -- Functon for tab completion
+
 local has_words_before = function()
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -29,47 +72,71 @@ cmp.setup({
         end,
     },
     window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        -- Default Options
+        -- cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+
+        completion = {
+            border = 'single',
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+        },
+        documentation = {
+            border = 'single',
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+        },
+    },
+    formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, item)
+            item.kind = string.format('%s', icons[item.kind])
+            item.menu = ({
+                buffer = '[Buffer]',
+                luasnip = '[Snip]',
+                nvim_lsp = '[LSP]',
+                nvim_lua = '[API]',
+                path = '[Path]',
+                rg = '[RG]',
+            })[entry.source.name]
+            return item
+        end,
     },
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
-        elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-        elseif has_words_before() then
-            cmp.complete()
-        else
-            fallback()
-        end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-        else
-            fallback()
-        end
-    end, { "i", "s" }),
-
+    -- Accept currently selected item. Set `select` to `false` to only
+    -- confirm explicitly selected items.
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<ESC'] = cmp.mapping.abort(),
-
-        -- Accept currently selected item. Set `select` to `false` to only
-        -- confirm explicitly selected items.
-
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with
+                -- expand_or_locally_jumpable() -- they way you will only jump
+                -- inside the snippet region
+
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     }),
 
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', priority = 5 },
+        { name = 'nvim_lsp', priority = 5, options = { show_autosnippets = false } },
         { name = 'buffer', priority = 4 },
         { name = 'luasnip', priority = 3 },
         { name = 'path', priority = 2 },
