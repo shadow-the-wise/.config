e847b2d7823dddefebefabbe65c600982ca9d80c
shadowchaser04 <shadowchasing@icloud.com>
2023-01-30 14:55:37 +0000

    ADDED: icons for nvim

---
+++ nvim/plugin/nvim-devicons.lua
@@ -0,0 +1,5 @@
+require 'nvim-web-devicons'.setup {
+    -- globally enable different highlight colors per icon (default to true)
+    -- if set to false all icons will have the default icon's color
+    color_icons = true;
+}
