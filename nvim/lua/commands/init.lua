d07ea617c98407fd2fd0a9c4ba5579f7aacd93cb
shadowchaser04 <shadowchasing@icloud.com>
2023-01-30 14:54:19 +0000

    ADDED: per directory init file.

---
+++ nvim/lua/commands/init.lua
@@ -0,0 +1,14 @@
+-- spell file
+require('commands.spell')
+
+-- basic configuration settings
+require('commands.basic')
+
+-- quit auto commands
+require('commands.quits')
+
+-- skeleton files
+require('commands.skeletons')
+
+-- autocorrect
+require('commands.autocorrect')
---
+++ nvim/lua/keymaps/init.lua
@@ -0,0 +1,13 @@
+require('keymaps.change_list')
+
+require('keymaps.hop')
+
+require('keymaps.jump_list')
+
+require('keymaps.navigation')
+
+require('keymaps.resize')
+
+require('keymaps.search')
+
+require('keymaps.telescope')
