c04f2e8be34763221ff10e36e20fdb08ec14a5cd
shadowchaser04 <shadowchasing@icloud.com>
2023-01-31 00:24:42 +0000

    ADDED: a pcall to each init.lua require

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,3 +1,28 @@
-require('lsp.lsp_config')
-require('lsp.handlers')
-require('lsp.diagnostic')
+-- [LSP] Language server configuration.
+
+-- nvim language server protocal configuration
+
+local lsp_config_ok, lsp_config_response = pcall(require, 'lsp.lsp_config')
+if not lsp_config_ok then
+    vim.notify(lsp_config_response, vim.log.levels.ERROR)
+else
+    require('lsp.lsp_config')
+end
+
+-- nvim language server protocal handlers
+
+local handlers_ok, handlers_response = pcall(require, 'lsp.handlers')
+if not handlers_ok then
+    vim.notify(handlers_response, vim.log.levels.ERROR)
+else
+    require('lsp.handlers')
+end
+
+-- nvim language server protocal diagnostic
+
+local diagnostic_ok, diagnostic_response = pcall(require, 'lsp.diagnostic')
+if not diagnostic_ok then
+    vim.notify(diagnostic_response, vim.log.levels.ERROR)
+else
+    require('lsp.diagnostic')
+end
