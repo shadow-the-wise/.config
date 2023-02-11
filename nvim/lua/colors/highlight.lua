6835dd945138737528c33537fb7cca925e97e510
shadowchaser04 <shadowchasing@icloud.com>
2023-02-11 10:17:57 +0000

    ADDED: lua highlight group

---
+++ nvim/lua/colors/highlight.lua
@@ -0,0 +1,3 @@
+-- Highlight yank remove underline (it removes the underline from the second
+-- result on increemtal highlight"
+vim.api.nvim_set_hl(0, 'IncSearch', { fg = "#eceff4", bg = "#5e81ac" })
