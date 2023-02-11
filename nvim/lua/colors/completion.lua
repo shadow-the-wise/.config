dc9ba2f75c0e89d633d98bbdf74c117bdc57772b
shadowchaser04 <shadowchasing@icloud.com>
2023-02-11 10:17:28 +0000

    ADDED: Lua completion colors

---
+++ nvim/lua/colors/completion.lua
@@ -0,0 +1,17 @@
+-- [Pum]
+
+-- Pum Menu back color
+vim.api.nvim_set_hl(0, 'Pmenu', { fg = "#ECEFF4" })
+vim.api.nvim_set_hl(0, 'PmenuSel', { bg = "#4c566a" })
+
+-- Fuzzy Match blue
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { fg = "#5E81AC" })
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { fg = "#5E81AC" })
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { fg = "#5E81AC" })
+
+-- [Icons]
+
+-- Functions and Methods pink
+vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { fg = "#B48EAD" })
+vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { fg = "#B48EAD" })
+vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { fg = "#B48EAD" })
