c272a3d1b8ac043d13ecd9fdb76b73f3b5407945
shadowchaser04 <shadowchasing@icloud.com>
2023-02-11 10:24:00 +0000

    ADDED: Lua Hop highlight Group

---
+++ nvim/lua/colors/hop.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_set_hl(0, 'lsp_document_highlight', { bg = "#ECEFF4" })
+
+-- [Hop]
+
+-- blue
+vim.api.nvim_set_hl(0, 'HopNextKey', { fg = "#EBCB8B" })
+
+-- yellow
+vim.api.nvim_set_hl(0, 'HopNextKey1', { fg = "#81A1C1" })
