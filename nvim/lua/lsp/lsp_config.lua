3107dfe1d7f08e0cedfb9072889733bb7ca10f8c
shadowchaser04 <shadowchasing@icloud.com>
2023-02-27 01:11:23 +0000

    RENAME: sumneko changed to lua_ls

--- nvim/lua/lsp/lsp_config.lua
+++ nvim/lua/lsp/lsp_config.lua
@@ -122,7 +122,7 @@ require('lspconfig')["solargraph"].setup {
 
 -- [LUA]
 
-require('lspconfig')["sumneko_lua"].setup {
+require('lspconfig')["lua_ls"].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     capabilities = capabilities,
